7f3f014064b33ec453ad7fa1b03caf8f5d9e5889
Piero <piero.onorati@virgilio.it>
2021-05-26 16:21:17 +0200

    add semicolons  and corrections

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
-var nome = prompt('Inserisci il tuo Nome')
-// console.log(nome)
-// console.log(nome.trim())
+var nome = prompt('Inserisci il tuo Nome');
+// console.log(nome);
+// console.log(nome.trim());
 
-var cognome = prompt('Inserisci il tuo Cognome')
-// console.log(cognome)
-// console.log(cognome.trim())
+var cognome = prompt('Inserisci il tuo Cognome');
+// console.log(cognome);
+// console.log(cognome.trim());
 
-var colore = prompt('Inserisci il tuo Colore Preferito')
-// console.log(colore)
-// console.log(colore.trim())
+var colore = prompt('Inserisci il tuo Colore Preferito');
+// console.log(colore);
+// console.log(colore.trim());
 
-var password = nome.trim() + cognome.trim() + colore.trim() + '21'
-// console.log(password)
+var password = nome.trim() + cognome.trim() + colore.trim() + '21';
+// console.log(password);
+
+var emoji = String.fromCodePoint(0x1F60E);
+// console.log(emoji);
 
 var password = password.toLowerCase()
-alert('La tua password è: ' + password)
+// console.log(password);
+
+document.getElementById("password").innerHTML = "La tua Password è:" + password + emoji;
